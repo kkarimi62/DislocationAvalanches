93d7d6d9ae36919ac149e3218ed21178007d991f
kamran.karimi1 <kkarimi62>
2022-06-17 17:40:28 -0600

    bash file updated

--- nanoindentation/ebsd/mtex-5.8.1/ebsd.sh
+++ nanoindentation/ebsd/mtex-5.8.1/ebsd.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+exec=/home/kamran.karimi1/Project/git/DislocationAvalanches/nanoindentation/ebsd
 module load matlab/r2017b
 
+python ${exec}/indent_xy.py
 #-nojvm -nosplash -nodesktop -r
-matlab -nodisplay -r "try, run('../ebsd_matlab'), catch e, disp(getReport(e)), exit(1), end, exit(0);"
+matlab -nodisplay -r "try, run('${exec}/ebsd_matlab'), catch e, disp(getReport(e)), exit(1), end, exit(0);"
 echo "matlab exit code: $?"
 
