ebba5906f23dab793908a726cfd31bc5a7abf1f7
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-08-21 19:52:36 +0200

    updates

--- tensile/python/postprocess.py
+++ tensile/python/postprocess.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
  							}['1'] #--- source
 	EXEC_DIR = '.'     #--- path for executable file
 	durtn = '23:59:59'
-	mem = '16gb'
+	mem = '8gb'
 	partition = ['parallel','cpu2019','bigmem','single'][3] 
 	argv = "%s"%(readPath) #--- don't change! 
 	PYFILdic = { 
