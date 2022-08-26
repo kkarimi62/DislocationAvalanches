d2166690d7503bd6eeab6f64d0fb6d19873952b2
kkarimi62 <kamran.karimi1362@gmail.com>
2022-08-26 14:29:25 +0200

    updates

--- gnn/gnn.py
+++ gnn/gnn.py
@@ -6,7 +6,7 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL):
 	confParser.read('config.ini')
 	#--- set parameters
 	confParser.set('Parameters','num_processing_steps_tr','3')
-	confParser.set('Parameters','num_training_iterations','8000')
+	confParser.set('Parameters','num_training_iterations','10000')
 	confParser.set('Parameters','learning_rate','2.5e-03')
 	#
 	confParser.set('gnn library path','gnnLibDir',os.getcwd()+'/./hs_implementation')
@@ -32,13 +32,13 @@ if __name__ == '__main__':
 #
 	nruns	 = range(1)
 	jobname  = {
-					1:'test7th',
+					1:'predictHardness',
 				}[1]
 	DeleteExistingFolder = True
 	EXEC_DIR = '.'     #--- path for executable file
-	durtn = '23:59:59'
-	mem = '16gb'
-	partition = ['cpu2019','bigmem','parallel','single'][1]
+	durtn = '47:59:59'
+	mem = '8gb'
+	partition = ['cpu2019','bigmem','parallel','single'][0]
 	PYFILdic = { 
 		0:'gnnPolyCryst.ipynb',
 		1:'gnnPolyCryst.py',
