c4acd98cf780534f892e48e71dbc8af666fd378c
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-08-27 09:40:06 +0200

    update

--- gnn/submitPY.py
+++ gnn/submitPY.py
@@ -3,14 +3,14 @@ if __name__ == '__main__':
 	import os
 	import numpy as np
 	#---
-	lnums = [ 35,9 ]
+	lnums = [ 35,8 ]
 	string=open('gnn.py').readlines() #--- python script
 	#---
-#	num_processing_steps_tr=range(1,10)
-	num_training_iterations=[10000,100000,1000000]
+	num_processing_steps_tr=range(1,10)
+#	num_training_iterations=[10000,100000,1000000]
 
-#	PHI=dict(zip(range(len(num_processing_steps_tr)),num_processing_steps_tr))
-	PHI=dict(zip(range(len(num_training_iterations)),num_training_iterations))
+	PHI=dict(zip(range(len(num_processing_steps_tr)),num_processing_steps_tr))
+#	PHI=dict(zip(range(len(num_training_iterations)),num_training_iterations))
 	nphi = len(PHI)
 	#---
 	count = 0
@@ -18,11 +18,11 @@ if __name__ == '__main__':
 		val = PHI[key]
 		#---	
 		inums = lnums[ 0 ] - 1
-		string[ inums ] = "\t1:\'niter%s\',\n" % (key) #--- change job name
+		string[ inums ] = "\t1:\'nprocessing%s\',\n" % (key) #--- change job name
 		#---	densities
 		inums = lnums[ 1 ] - 1
-#		string[ inums ] = "\tconfParser.set(\'Parameters\',\'num_processing_steps_tr\',\'%s\')\n"%(val)
-		string[ inums ] = "\tconfParser.set(\'Parameters\',\'num_training_iterations\',\'%s\')\n"%(val)
+		string[ inums ] = "\tconfParser.set(\'Parameters\',\'num_processing_steps_tr\',\'%s\')\n"%(val)
+#		string[ inums ] = "\tconfParser.set(\'Parameters\',\'num_training_iterations\',\'%s\')\n"%(val)
 
 #		inums = lnums[ 2 ] - 1
 #		string[ inums ] = "\tconfParser.set(\'parameters\',\'load\',\'%s\')\n"%(val)
