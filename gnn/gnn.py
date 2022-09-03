fb0b5fabdfb7b69d4b5f47e8c0d9ce2779f4fdb7
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-09-03 16:39:23 +0200

    update

--- gnn/gnn.py
+++ gnn/gnn.py
@@ -8,6 +8,7 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL):
 	confParser.set('Parameters','num_processing_steps_tr','3')
 	confParser.set('Parameters','num_training_iterations','100000')
 	confParser.set('Parameters','learning_rate','1.0e-03')
+	confParser.set('Parameters','attributes','x y perimeter boundarySize numNeighbors')
 	#
 	confParser.set('gnn library path','gnnLibDir',os.getcwd()+'/./hs_implementation')
 	#
