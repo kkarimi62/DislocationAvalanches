0b9874026847f97c763b5221a97a72800161ae66
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-10-14 18:12:56 +0200

    update

--- gnn/gnn.py
+++ gnn/gnn.py
@@ -6,14 +6,13 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL):
 	confParser.read('config.ini')
 	#--- set parameters
 	confParser.set('Parameters','num_processing_steps_tr','3')
-	confParser.set('Parameters','num_training_iterations','50000')
+	confParser.set('Parameters','num_training_iterations','100000')
 	confParser.set('Parameters','learning_rate','1.0e-03')
-#	confParser.set('Parameters','attributes','x y grainSize perimeter boundarySize numNeighbors')
-	confParser.set('Parameters','attributes','x y')
+	confParser.set('Parameters','attributes','x y grainSize perimeter boundarySize numNeighbors')
 	#
 	confParser.set('flags','train_test','False')
-	confParser.set('flags','learning_curve','True')
-	confParser.set('flags','validation_curve','False')
+	confParser.set('flags','learning_curve','False')
+	confParser.set('flags','validation_curve','True')
 	confParser.set('flags','remote_machine','True')
 	#
 	confParser.set('gnn library path','gnnLibDir',os.getcwd()+'/./hs_implementation')
@@ -40,8 +39,8 @@ if __name__ == '__main__':
 	nruns	 = range(1)
 	jobname  = {
 					1:'predictHardnessForcesAddedLearningCurve4th',
-					2:'predictHardnessForcesAddedValidation',
-				}[1]
+					2:'validation',
+				}[2]
 	DeleteExistingFolder = True
 	EXEC_DIR = '.'     #--- path for executable file
 	durtn = '23:59:59'
