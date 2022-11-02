780923221c0b0c9d38a81e1cf5a8805718bbe4fb
kkarimi62 <kamran.karimi1362@gmail.com>
2022-11-02 09:49:17 +0100

    updates

--- nanoindentation/ebsd/ebsd_matlab.m
+++ nanoindentation/ebsd/ebsd_matlab.m
@@ -63,8 +63,8 @@ mori = inv(grains(pairs(:,1)).meanOrientation) .* grains(pairs(:,2)).meanOrienta
 %---- print attributes
 % open your file for writing
 fid = fopen('attributes.txt','wt');
-fprintf(fid,'#grainID x y area perimeter subBoundaryLength diameter equivalentPerimeter shapeFactor isBoundary hasHole isInclusion numNeighbors\n');
-fprintf(fid,'%d %e %e %e %e %e %e %e %e %d %d %d %d\n', transpose([grains.id grains.centroid grains.area grains.perimeter grains.subBoundaryLength grains.diameter grains.equivalentPerimeter grains.shapeFactor grains.isBoundary grains.hasHole grains.isInclusion grains.numNeighbors]));
+fprintf(fid,'#grainID x y area perimeter subBoundaryLength diameter equivalentPerimeter shapeFactor isBoundary hasHole isInclusion numNeighbors boundary\n');
+fprintf(fid,'%d %e %e %e %e %e %e %e %e %d %d %d %d %d\n', transpose([grains.id grains.centroid grains.area grains.perimeter grains.subBoundaryLength grains.diameter grains.equivalentPerimeter grains.shapeFactor grains.isBoundary grains.hasHole grains.isInclusion grains.numNeighbors grains.boundary]));
 fclose(fid);
 
 % open your file for writing
