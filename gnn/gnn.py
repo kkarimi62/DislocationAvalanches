89c3c1f34b864eccf74bb6860a2c116a51033e35
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-11-04 11:15:26 +0100

    update

--- gnn/gnn.py
+++ gnn/gnn.py
@@ -14,8 +14,8 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL,seed):
 	confParser.set('Parameters','n_cross_val','5')
 	confParser.set('Parameters','seed','%s'%seed)
 	#
-	confParser.set('flags','train_test','True')
-	confParser.set('flags','learning_curve','False')
+	confParser.set('flags','train_test','False')
+	confParser.set('flags','learning_curve','True')
 	confParser.set('flags','validation_curve','False')
 	confParser.set('flags','remote_machine','True')
 	#
@@ -41,12 +41,12 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL,seed):
 if __name__ == '__main__':
 	import os
 	import numpy as np
-#
-	nruns	 = range(10)
+	##
+	nruns	 = range(1)
 	jobname  = {
 					1:'hardness_best_train',
 					2:'learning_curve', #'model_validation', #'learning_curve',
-				}[1]
+				}[2]
 	DeleteExistingFolder = True
 	EXEC_DIR = '.'     #--- path for executable file
 	durtn = '23:59:59'
