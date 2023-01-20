76f27e8ac693caddc691aa6a450694e9d8db134c
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-01-20 09:08:26 +0100

    update

--- tensile/python/postproc_ncbj_slurm.py
+++ tensile/python/postproc_ncbj_slurm.py
@@ -28,11 +28,13 @@ if __name__ == '__main__':
 	nThreads = 1
 	jobname  = {
 				'1':'tensileCantor_tensile900_rate2', 
-				}['1']
+				'2':'tensileCantorT300KRateE3', 
+				}['2']
 	DeleteExistingFolder = True
 	readPath = os.getcwd() + {
 								'1':'/../testdata/aedata/cantor/rateT900K/rate2',
- 							}['1'] #--- source
+								'2':'/../testdata/aedata/cantor/temperaturesRateE3/temp300',
+ 							}['2'] #--- source
 	EXEC_DIR = '.'     #--- path for executable file
 	durtn = '23:59:59'
 	mem = '32gb'
