9769561f59ad763a9d845141245463d76ed814fd
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-02-05 21:54:25 +0100

    update

--- tensile/python/postproc_ncbj_slurm.py
+++ tensile/python/postproc_ncbj_slurm.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
 	jobname  = {
 				'1':'tensileCantor_tensile900_rate2', 
 				'2':'tensileCantorT300KRateE8', 
-				'3':'tensileCantor_tensile900_rate4/kernel0', 
+				'3':'tensileCantor_tensile900_rate4_kernels/kernel0', 
 				}['3']
 	DeleteExistingFolder = True
 	readPath = os.getcwd() + {
