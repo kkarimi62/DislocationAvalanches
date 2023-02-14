19c4de205e25c87ad80d0c03832cca56d91e863f
kkarimi62 <kamran.karimi1362@gmail.com>
2023-02-14 15:34:44 +0100

    updates

--- tensile/simulations/simulations.py
+++ tensile/simulations/simulations.py
@@ -4,11 +4,8 @@ def makeOAR( EXEC_DIR, node, core, time ):
 	print >> someFile, 'EXEC_DIR=%s\n' %( EXEC_DIR )
 	print >> someFile, 'MEAM_library_DIR=%s\n' %( MEAM_library_DIR )
 	print >> someFile, 'module load gcc/7.3.0\nmodule load openmpi/4.0.2-gnu730\nmodule load lib/openblas/0.3.13-gnu\n'
-	#--- run python script 
 
 	#--- run python script 
-#	 print >> someFile, "$EXEC_DIR/%s < in.txt -var OUT_PATH %s -var MEAM_library_DIR %s"%( EXEC, OUT_PATH, MEAM_library_DIR )
-#	cutoff = 1.0 / rho ** (1.0/3.0)
 	for script,var,indx, execc in zip(Pipeline,Variables,range(100),EXEC):
 		if execc == 'lmp': #_mpi' or EXEC == 'lmp_serial':
 			print >> someFile, "mpirun --oversubscribe -np %s $EXEC_DIR/%s < %s -echo screen -var OUT_PATH \'%s\' -var PathEam %s -var INC \'%s\' %s\n"%(nThreads*nNode,EXEC_lmp, script, OUT_PATH, '${MEAM_library_DIR}', SCRPT_DIR, var)
@@ -24,7 +21,7 @@ if __name__ == '__main__':
 		import os
 		import numpy as np
 
-		nruns	 = range(1)
+		nruns	 = range(24)
 		#
 		nThreads = 8
 		nNode	 = 1
@@ -86,7 +83,7 @@ if __name__ == '__main__':
 		def SetVariables():
 			Variable = {
 					0:' -var natoms 100000 -var cutoff 3.52 -var ParseData 0 -var ntype 3 -var DumpFile dumpInit.xyz -var WriteData data_init.txt',
-					6:' -var buff 0.0 -var T 300 -var P 0.0 -var gammaxy 1.0 -var gammadot 2.0e-04 -var ndump 1000 -var ParseData 1 -var DataFile Equilibrated_300.dat -var DumpFile dumpSheared.xyz',
+					6:' -var buff 0.0 -var T 300 -var P 0.0 -var gammaxy 1.0 -var gammadot 1.0e-04 -var ndump 1000 -var ParseData 1 -var DataFile Equilibrated_300.dat -var DumpFile dumpSheared.xyz',
 					4:' -var T 600.0 -var t_sw 20.0 -var DataFile Equilibrated_600.dat -var nevery 100 -var ParseData 1 -var WriteData swapped_600.dat', 
 					5:' -var buff 0.0 -var nevery 1000 -var ParseData 0 -var natoms 10000 -var ntype 5 -var cutoff 3.54  -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt -var seed0 %s -var seed1 %s -var seed2 %s -var seed3 %s'%tuple(np.random.randint(1001,9999,size=4)), 
 					51:' -var buff 0.0 -var nevery 1000 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt', 
--- tensile/simulations/submitPY.py
+++ tensile/simulations/submitPY.py
@@ -3,10 +3,12 @@ if __name__ == '__main__':
 	import os
 	import numpy as np
 	#---
-	lnums = [ 33, 93, 89   ]
-	string=open('simulations-ncbj.py').readlines() #--- python script
+	lnums = [ 30, 86   ]
+#	string=open('simulations-ncbj.py').readlines() #--- python script
+	string=open('simulations.py').readlines() #--- python script
 	#---
-	PHI  = dict(zip(range(4),[600,700,800,900]))
+#	PHI  = dict(zip(range(4),[600,700,800,900]))
+	PHI  = dict(zip(range(3),[0.5e-4,4e-4,8e-4]))
 	nphi = len(PHI)
 	#---
 	#---
@@ -15,10 +17,13 @@ if __name__ == '__main__':
 	keyss= list(PHI.keys())
 	keyss.sort()
 	for iphi in keyss:
-			#---	
+			#---
+			temp = 300 #int(PHI[iphi])
+			rate = PHI[iphi]
+			rate_id = iphi
 			#---	densities
 				inums = lnums[ 0 ] - 1
-				string[ inums ] = "\t3:\'CantorNatom10KTemp%sKRate1e8\',\n"%(int(PHI[iphi])) #--- change job name
+				string[ inums ] = "\t3:\'CantorNatom10KTemp%sKRate%s\',\n"%(temp,rate_id) #--- change job name
 			#---
 				inums = lnums[ 1 ] - 1
 				string[ inums ] = "\t7:\' -var buff 0.0 -var T %s -var P 0.0 -var nevery 1000 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated_%s.dat\',\n"%(int(PHI[iphi]),int(PHI[iphi]))
