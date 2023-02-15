415179d4ccacd800b7225f2fc80b51df86358c86
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-02-15 11:40:28 +0100

    updates

--- tensile/python/postproc_ncbj_slurm.py
+++ tensile/python/postproc_ncbj_slurm.py
@@ -24,7 +24,7 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv):
 if __name__ == '__main__':
 	import os
 #
-	runs	 = range(8)
+	runs	 = range(24)
 	nNode    = 1
 	nThreads = 1
 	jobname  = {
