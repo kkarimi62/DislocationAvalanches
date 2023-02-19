137a179090fb99a05a4caf6e506c0588d9a285ef
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-02-19 09:46:38 +0100

    updates

--- tensile/simulations/submitPY.py
+++ tensile/simulations/submitPY.py
@@ -3,28 +3,28 @@ if __name__ == '__main__':
 	import os
 	import numpy as np
 	#---
-#	lnums = [ 30, 90, 86   ]
-#	string=open('simulations.py').readlines() #--- python script
-	lnums = [ 33, 93, 89   ]
-	string=open('simulations-ncbj.py').readlines() #--- python script
+	lnums = [ 30, 90, 86   ]
+	string=open('simulations.py').readlines() #--- python script
+#	lnums = [ 33, 93, 89   ]
+#	string=open('simulations-ncbj.py').readlines() #--- python script
 	#---
 	Temps  = {
-#				0:300,
+				0:300,
 #				1:600,
 #				2:700,
 #				3:800,
 #				4:900,
-				5:1200,
-				6:1400,
-				7:1600,
+#				5:1200,
+#				6:1400,
+#				7:1600,
 			}
 	Rates  = {
 #				0:0.5e-4,
-				1:1e-4,
+#				1:1e-4,
 #				2:4e-4,
 #				3:8e-4
 #				4:8e-3,
-#				5:8e-2,
+				5:8e-2,
 			}
 	#---
 	count = 0
@@ -35,8 +35,8 @@ if __name__ == '__main__':
 				rate = Rates[keys_r]
 			#---	densities
 				inums = lnums[ 0 ] - 1
-#				string[ inums ] = "\t3:\'CantorNatom10KTemp%sKMultipleRates/Rate%s\',\n"%(temp,keys_r) #--- change job name
-				string[ inums ] = "\t3:\'CantorNatom10KMultipleTemp/Temp%sK\',\n"%(temp) #--- change job name
+				string[ inums ] = "\t3:\'CantorNatom10KTemp%sKMultipleRates/Rate%s\',\n"%(temp,keys_r) #--- change job name
+#				string[ inums ] = "\t3:\'CantorNatom10KMultipleTemp/Temp%sK\',\n"%(temp) #--- change job name
 			#---
 				inums = lnums[ 1 ] - 1
 				string[ inums ] = "\t7:\' -var buff 0.0 -var T %s -var P 0.0 -var nevery 1000 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated_%s.dat\',\n"%(temp,temp)
