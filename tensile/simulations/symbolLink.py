246c08b64fc894593e4bb946a792ac1f3f1a3de6
kkarimi62 <kamran.karimi1362@gmail.com>
2023-02-20 08:32:57 +0100

    updates

--- tensile/simulations/symbolLink.py
+++ tensile/simulations/symbolLink.py
@@ -4,12 +4,12 @@ if __name__ == '__main__':
 	import os
 	import sys
 	#--- 
-	jobname  = 'test3rd'
-	job_id = 9489151 
-	N = 1
+	jobname  = 'CantorNatom10KTemp300KMultipleRates/Rate4'
+	job_id = 18054125
+	N = 60
 	#---
 	job_ids = [ job_id + i for i in xrange( N ) ]
 	for id_job, counter in zip( job_ids, xrange( sys.maxint ) ):
 		writPath = os.getcwd() + '/%s/Run%s' % ( jobname, counter ) # --- curr. dir
-		for file_name in [ 'virialStress.xyz' ]:
+		for file_name in [ 'dumpSheared.xyz' ]:
 			os.system( 'ln -s /scratch/%s/%s %s/' % ( id_job, file_name, writPath ) )
