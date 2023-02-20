7ef8de949336eb545c2a69e98952dff9b4022e96
kkarimi62 <kamran.karimi1362@gmail.com>
2023-02-20 12:00:43 +0100

    updates

--- tensile/python/submitPY.py
+++ tensile/python/submitPY.py
@@ -3,10 +3,10 @@ if __name__ == '__main__':
 	import os
 	import numpy as np
 	#---
-#	lnums = [ 34, 41 ]
-	lnums = [ 28, 32 ]
-#	string=open('postproc_ncbj_slurm.py').readlines() #--- python script
-	string=open('postprocess.py').readlines() #--- python script
+	lnums = [ 34, 41 ]
+#	lnums = [ 28, 32 ]
+	string=open('postproc_ncbj_slurm.py').readlines() #--- python script
+#	string=open('postprocess.py').readlines() #--- python script
 	#---
 #	PHI  = dict(zip(range(5),[10**1,10**2,10**3,10**4,10**5]))
 #	PHI  = dict(zip(range(4),[1,2,3,4]))
@@ -14,22 +14,22 @@ if __name__ == '__main__':
 #	kernel_width = dict(zip(range(4),[10**4,10**4,10**4,10**4]))
 
 	Temps  = {
-				0:300,
+#				0:300,
 #				1:600,
 #				2:700,
 #				3:800,
 #				4:900,
-#				5:1200,
+				5:1200,
 #				6:1400,
 #				7:1600,
 			}
 	Rates  = {
 #				0:0.5e-4,
-#				1:1e-4,
+				1:1e-4,
 #				2:4e-4,
 #				3:8e-4,
 #				4:8e-3,
-				5:8e-2,
+#				5:8e-2,
 			}
 	#---
 	count = 0
@@ -40,11 +40,13 @@ if __name__ == '__main__':
 				rate = Rates[keys_r]
 			#---	densities
 				inums = lnums[ 0 ] - 1
-				string[ inums ] = "\t\'3\':\'CantorNatom10KTemp300KMultipleRates/Rate%s\',\n"%(keys_r) #--- change job name
+#				string[ inums ] = "\t\'3\':\'CantorNatom10KTemp300KMultipleRates/Rate%s\',\n"%(keys_r) #--- change job name
+				string[ inums ] = "\t\'4\':\'CantorNatom10KMultipleTemp/Temp%sK\',\n"%(temp) #--- change job name
 		#---	densities
 				inums = lnums[ 1 ] - 1
 #				string[ inums ] = "\t\'1\':\'/../testdata/aedata/cantor/rateT900K/rate%s\',\n"%(int(PHI[key]))
-				string[ inums ] = "\t\'3\':\'/../simulations/CantorNatom10KTemp300KMultipleRates/Rate%s\',\n"%(keys_r)
+#				string[ inums ] = "\t\'3\':\'/../simulations/CantorNatom10KTemp300KMultipleRates/Rate%s\',\n"%(keys_r)
+				string[ inums ] = "\t\'4\':\'/../simulations/CantorNatom10KMultipleTemp/Temp%sK\',\n"%(temp)
 
 #				inums = lnums[ 1 ] - 1
 #				string[ inums ] = "\tconfParser.set(\'avalanche statistics\',\'kernel_width\',\'%s\'),\n"%(int(PHI[key]))
