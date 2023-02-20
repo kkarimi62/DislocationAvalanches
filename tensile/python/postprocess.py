66b0b74ac4920cd52e48e9f11a15ac3b74a02902
kkarimi62 <kamran.karimi1362@gmail.com>
2023-02-20 09:38:36 +0100

    updates

--- tensile/python/postprocess.py
+++ tensile/python/postprocess.py
@@ -23,7 +23,7 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv):
 if __name__ == '__main__':
 	import os
 #
-	runs	 = range(60)
+	runs	 = range(144)
 	jobname  = {
 				'3':'CantorNatom10KTemp300KMultipleRates/Rate5', 
 				}['3']
