59d8be6531509a71d9aa30d5b1bd6f1c3ce9ead8
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-02-28 19:05:02 +0100

    updates

--- tensile/python/configMaker.py
+++ tensile/python/configMaker.py
@@ -6,7 +6,7 @@ confParser.read('config.ini')
 #--- set parameters
 confParser.set('avalanche statistics','kernel_width','100')
 confParser.set('test data directory','path',sys.argv[1])
-confParser.set('py library directory','path',sys.argv[1]+'/../../../HeaDef/postprocess')
+confParser.set('py library directory','path','~/Project/git/HeaDef/postprocess')
 confParser.set('dislocation analysis','outputPath',sys.argv[2])
 #--- write
 confParser.write(open('config.ini','w'))	
