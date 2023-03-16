cf1c01116d0bf81e3e6c87c405fb3ce1aecdc18a
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-03-16 14:45:08 +0100

    updates

--- tensile/python/symbolLink.py
+++ tensile/python/symbolLink.py
@@ -4,13 +4,32 @@ if __name__ == '__main__':
 	import os
 	import sys
 	#--- 
-	jobname  = 'CantorNatom10KTemp300KMultipleRates/Rate3'
-	job_id = 18310818
-	N = 44
+	Rates  = {
+				0:0.5e-4,
+				3:8e-4,
+				4:8e-3,
+				5:8e-2,
+			}
+
+	nruns  = {
+				0:24,
+				3:44,
+				4:60,
+				5:144,
+			}
 	#---
-	job_ids = [ job_id + i for i in xrange( N ) ]
-	for id_job, counter in zip( job_ids, xrange( sys.maxint ) ):
-		writPath = os.getcwd() + '/%s/Run%s/dislocations' % ( jobname, counter ) # --- curr. dir
-		os.system('mkdir -p %s'%writPath)
-		for file_name in [ 'dislocations/structureCnaTypeFraction.txt' ]:
-			os.system( 'cp /scratch/%s/%s %s/' % ( id_job, file_name, writPath ) )
+	for keys_r in Rates:
+		rate = Rates[keys_r]
+		N = nruns[ keys_r ]
+
+		jobname  = 'CantorNatom10KTemp300KMultipleRates/Rate%s'%keys_r
+		job_id = int(open('%s/jobID.txt'%jobname).readline().split()[-1])
+		#---
+		job_ids = [ job_id + i for i in xrange( N ) ]
+		for id_job, counter in zip( job_ids, xrange( sys.maxint ) ):
+
+			writPath = os.getcwd() + '/%s/Run%s/dislocations' % ( jobname, counter ) # --- curr. dir
+			os.system('mkdir -p %s'%writPath)
+
+			for file_name in [ 'dislocations/structureCnaTypeFraction.txt' ]:
+				os.system( 'cp /scratch/%s/%s %s/' % ( id_job, file_name, writPath ) )
