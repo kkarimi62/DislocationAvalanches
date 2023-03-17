fd7dd590c197225953d05a34983c4810f45d8488
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-03-17 09:22:22 +0100

    updates

--- tensile/python/symbolLink.py
+++ tensile/python/symbolLink.py
@@ -6,23 +6,26 @@ if __name__ == '__main__':
 	#--- 
 	Rates  = {
 				0:0.5e-4,
-				3:8e-4,
-				4:8e-3,
-				5:8e-2,
+#				3:8e-4,
+#				4:8e-3,
+#				5:8e-2,
 			}
 
 	nruns  = {
 				0:24,
-				3:44,
-				4:60,
-				5:144,
+#				3:44,
+#				4:60,
+#				5:144,
 			}
+	alloy = 'Cantor'
+
+
 	#---
 	for keys_r in Rates:
 		rate = Rates[keys_r]
 		N = nruns[ keys_r ]
 
-		jobname  = 'CantorNatom10KTemp300KMultipleRates/Rate%s'%keys_r
+		jobname  = '%sNatom10KTemp300KMultipleRates/Rate%s'%(alloy,keys_r)
 		job_id = int(open('%s/jobID.txt'%jobname).readline().split()[-1])
 		#---
 		job_ids = [ job_id + i for i in xrange( N ) ]
@@ -33,3 +36,5 @@ if __name__ == '__main__':
 
 			for file_name in [ 'dislocations/structureCnaTypeFraction.txt' ]:
 				os.system( 'cp /scratch/%s/%s %s/' % ( id_job, file_name, writPath ) )
+
+		os.system("git add %sNatom10KTemp300KMultipleRates/Rate%s/Run*/dislocations/structureCnaTypeFraction.txt;git commit -m 'updates';git push"%(alloy,keys_r))
