bc24ab3d094a8f1330196ce60d4cfd21fa66f3be
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-07-08 18:08:10 +0200

    update

--- tensile/simulations/simulations.py
+++ tensile/simulations/simulations.py
@@ -42,12 +42,12 @@ if __name__ == '__main__':
 
     nruns	 = range(1) #24)
     #
-    nThreads =  4 #8
+    nThreads =  8 #4 #8
     nNode	 = 1
     #
     jobname  = {
                 3:'CantorNatom10KTemp300KMultipleRates/test', 
-                4:'test-intel',#'NiNatom10KTemp300KMultipleRates/Rate0', 
+                4:'test-mpi', #nicocrNatom10KTemp300KMultipleRates/Rate0', 
                }[4]
     sourcePath = os.getcwd() +\
                 {	
@@ -103,9 +103,9 @@ if __name__ == '__main__':
     def SetVariables():
         Variable = {
                 0:' -var natoms 100000 -var cutoff 3.52 -var ParseData 0 -var ntype 3 -var DumpFile dumpInit.xyz -var WriteData data_init.txt',
-                6:' -var buff 0.0 -var T 300 -var P 0.0 -var gammaxy 0.01 -var gammadot 1.0e-04 -var nthermo 10000 -var ndump 1000 -var ParseData 1 -var DataFile Equilibrated_300.dat -var DumpFile dumpSheared.xyz',
+                6:' -var buff 0.0 -var T 300 -var P 0.0 -var gammaxy 0.001 -var gammadot 0.5e-4 -var nthermo 10000 -var ndump 1000 -var ParseData 1 -var DataFile Equilibrated_300.dat -var DumpFile dumpSheared.xyz',
                 4:' -var T 600.0 -var t_sw 20.0 -var DataFile Equilibrated_600.dat -var nevery 100 -var ParseData 1 -var WriteData swapped_600.dat', 
-                5:' -var buff 0.0 -var nevery 1000 -var ParseData 0 -var natoms 10000 -var ntype 5 -var cutoff 3.54  -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt -var seed0 %s -var seed1 %s -var seed2 %s -var seed3 %s'%tuple(np.random.randint(1001,9999,size=4)), 
+                5:' -var buff 0.0 -var nevery 1000 -var ParseData 0 -var natoms 100000 -var ntype 5 -var cutoff 3.54  -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt -var seed0 %s -var seed1 %s -var seed2 %s -var seed3 %s'%tuple(np.random.randint(1001,9999,size=4)), 
                 51:' -var buff 0.0 -var nevery 1000 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt', 
                 7:' -var buff 0.0 -var T 300.0 -var P 0.0 -var seed %s -var nevery 1000 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated_300.dat'%(np.random.randint(10000,99999)),
                 71:' -var buff 0.0 -var T 0.1 -var P 0.0 -var seed %s -var nevery 100 -var ParseData 1 -var DataFile swapped_600.dat -var DumpFile dumpThermalized2.xyz -var WriteData Equilibrated_0.dat'%(np.random.randint(10000,99999)),
@@ -130,7 +130,7 @@ if __name__ == '__main__':
 #	Variables = list(map(lambda x:Variable[x], indices))
 #        print('EXEC=',EXEC)
     #
-    EXEC_lmp = ['lmp_mpi','lmp_serial','lmp_intel_cpu_intelmpi'][2]
+    EXEC_lmp = ['lmp_mpi','lmp_serial','lmp_intel_cpu_intelmpi'][0]
     durtn = ['95:59:59','01:59:59','167:59:59'][ 1 ]
     mem = '32gb'
     partition = ['gpu-v100','parallel','cpu2019','single'][2]
