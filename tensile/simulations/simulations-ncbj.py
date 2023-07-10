5c0ffe752c7781207a1b2435c7f451130f9225af
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-07-10 23:54:18 +0200

    update

--- tensile/simulations/simulations-ncbj.py
+++ tensile/simulations/simulations-ncbj.py
@@ -143,7 +143,7 @@ if __name__ == '__main__':
     
     
     #--- binary files
-    EXEC_lmp = ['lmp_g++_openmpi','lmp_intel_cpu_intelmpi','lmp_omp'][1]
+    EXEC_lmp = ['lmp_g++_openmpi','lmp_intel_cpu_intelmpi','lmp_omp'][0]
     durtn = ['95:59:59','00:59:59','167:59:59'][ 1 ]
     mem = '16gb'
     partition = ['INTEL_PHI','INTEL_CASCADE','INTEL_SKYLAKE','INTEL_IVY','INTEL_HASWELL'][0]
