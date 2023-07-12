625d4c19a8602cb437e508c9d3a3457af30ef342
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-07-12 14:55:30 +0200

    update

--- tensile/python/postprocess.py
+++ tensile/python/postprocess.py
@@ -1,6 +1,6 @@
 def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv):
 	#-- edit configMaker.py !!!
-	kernel_width = 100
+	kernel_width = 70
 	#--- set environment variables
 
 	someFile = open( 'oarScript.sh', 'w' )
@@ -16,13 +16,13 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv):
 if __name__ == '__main__':
 	import os
 #
-	runs	 = range(44) 
+	runs	 = range(24) 
 	jobname  = {
-				'3':'CantorNatom10KTemp300KMultipleRates/Rate3', 
+				'3':'CantorNatom10KTemp300KMultipleRates/Rate0', 
 				}['3']
 	DeleteExistingFolder = True
 	readPath = os.getcwd() + {
-								'3':'/../simulations/CantorNatom10KTemp300KMultipleRates/Rate3',
+								'3':'/../simulations/CantorNatom10KTemp300KMultipleRates/Rate0',
  							}['3'] #--- source
 	EXEC_DIR = '.'     #--- path for executable file
 	home_directory = os.path.expanduser( '~' )
