852756b5075aa6f6ac25a800741898312eaa3e12
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-07-12 15:30:36 +0200

    update

--- tensile/python/postprocess.py
+++ tensile/python/postprocess.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     keyno = 1
     convert_to_py = True
     absoluteOutputPath = [os.getcwd(),'/tmp'][0] #--- directory where py scripts are copied to.
-    outputPath = ['.','/scratch/$SLURM_JOB_ID'][1] #--- outputs will be in this directory
+    outputPath = ['.','/scratch/$SLURM_JOB_ID'][0] #--- outputs will be in this directory
     #---
     #---
     PYFIL = PYFILdic[ keyno ] 
