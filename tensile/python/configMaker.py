d63b0fb94e2e1a1603587313c2e506deac59273c
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-07-17 17:18:40 +0200

    update

--- tensile/python/configMaker.py
+++ tensile/python/configMaker.py
@@ -8,7 +8,7 @@ confParser.set('avalanche statistics','kernel_width',sys.argv[4])
 confParser.set('test data directory','path',sys.argv[2])
 confParser.set('py library directory','path',sys.argv[1])
 confParser.set('dislocation analysis','outputPath',sys.argv[3])
-    confParser.set('avalanche statistics','filter_fp',sys.argv[5])
+confParser.set('avalanche statistics','filter_fp',sys.argv[5])
 #--- write
 confParser.write(open('config.ini','w'))	
 
