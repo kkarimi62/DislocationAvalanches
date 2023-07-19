19a15b4b334ca2db15749eecb379116e97be1bf5
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-07-19 23:05:47 +0200

    update

--- tensile/python/postproc_ncbj_slurm.py
+++ tensile/python/postproc_ncbj_slurm.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
     py_library_directory = '%s/Project/git/HeaDef/postprocess'%home_directory
     durtn = '00:14:59'
     mem = '8gb'
-    partition = ['INTEL_PHI','INTEL_CASCADE','INTEL_SKYLAKE','INTEL_IVY','INTEL_HASWELL'][0]
+    partition = ['INTEL_PHI','INTEL_CASCADE','INTEL_SKYLAKE','INTEL_IVY','INTEL_HASWELL'][1]
     argv = "%s %s"%(py_library_directory,readPath) #--- don't change! 
     PYFILdic = { 
         0:'avalancheAnalysis.ipynb',
