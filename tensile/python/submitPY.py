afbbe0df463b372e6ccb8906bdd1be2450104722
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-07-20 11:19:20 +0200

    update

--- tensile/python/submitPY.py
+++ tensile/python/submitPY.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
 #	string=open('postprocess.py').readlines() #--- python script
 #	lnums = [ 21, 25, 3, 19 ]
     string=open('postproc_ncbj_slurm.py').readlines() #--- python script
-    lnums = [ 29,37,4,21 ]
+    lnums = [ 30,38,11,21,22 ]
     #---
 
     Temps  = {
@@ -20,9 +20,11 @@ if __name__ == '__main__':
 #				7:1600,
             }
 
-    kernel_widths  = dict(zip(range(8),np.logspace(2,7,6,base=2,dtype=int)))
-#                        { 
-#                         0:10,
+optimal_kernels={0:0,1:1}
+
+#     kernel_widths  = #dict(zip(range(8),np.logspace(2,7,6,base=2,dtype=int)))
+#                         { 
+#                          0:10,
 #                         1:20,
 #                         2:30,
 #                         3:40,
@@ -47,33 +49,33 @@ if __name__ == '__main__':
 #					}
 
     Rates  = {
-                12:0.125e-4,
+#                12:0.125e-4,
 #				11:0.250e-4,
-#				0:0.5e-4,
+				0:0.5e-4,
 #				3:8e-4,
 #				4:8e-3,
 #				5:8e-2,
             }
 
     nruns  = {
-                12:12,
+#                12:12,
 #				11:12,
-#				0:24,
+				0:24,
 #				3:44,
 #				4:60,
 #				5:144,
             }
 
     fixed_kernel_widths  = { 
-                        12:13,#70,
+#                        12:13,#70,
 #						11:13,#70,
-#						0:13,#70,
+						0:13,#70,
 #						3:13,#70,
 #						4:70,#70,
 #						5:30,#40,
                     }
 
-    alloy = 'nicocr'
+    alloy = 'Ni'
 
     #---
     count = 0
@@ -83,9 +85,9 @@ if __name__ == '__main__':
             #---
                 rate = Rates[keys_r]
                 nrun = nruns[ keys_r ]
-                kernel_width = fixed_kernel_widths[keys_r]
-                for keys_k in kernel_widths:
-                    kernel_width = kernel_widths[keys_k]
+#                kernel_width = fixed_kernel_widths[keys_r]
+                for keys_k in optimal_kernels:
+#                    kernel_width = kernel_widths[keys_k]
             #---	write to
                     inums = lnums[ 0 ] - 1
 #					string[ inums ] = "\t\'5\':\'%sNatom10KTemp300KMultipleRates/Rate%s\',\n"%(alloy,keys_r) #--- change job name
@@ -96,12 +98,13 @@ if __name__ == '__main__':
                     string[ inums ] = "\t\'5\':\'/../simulations/%sNatom10KTemp300KMultipleRates/Rate%s\',\n"%(alloy,keys_r)
 
                     inums = lnums[ 2 ] - 1
-                    string[ inums ] = "    kernel_width=%s\n"%(int(kernel_width))
+#                    string[ inums ] = "    kernel_width=%s\n"%(int(kernel_width))
+                    string[ inums ] = "\tprint(\'python3 configMaker.py %%s %%s %%s %%s/optimal_filtr_k%s.txt\n'%%(argv,outputPath,kernel_width,current_directory),file=someFile)\n"%keys_k
             #
                     inums = lnums[ 3 ] - 1
                     string[ inums ] = "    runs = range(%s)\n"%(nrun)
             #
                     sfile=open('junk%s.py'%count,'w');sfile.writelines(string);sfile.close()
                     os.system( 'python3 junk%s.py'%count )
-                    os.system( 'rm junk%s.py'%count )
+#                    os.system( 'rm junk%s.py'%count )
                     count += 1
