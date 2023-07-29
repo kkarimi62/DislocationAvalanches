8daede12c81ef0d07c10d6674086d9135f55f984
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-07-29 09:36:16 +0200

    update

--- tensile/simulations/simulations.py
+++ tensile/simulations/simulations.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     nNode	 = 1
     #
     jobname  = {
-                3:'CantorNatom10KTemp300KMultipleRates/Rate11', 
+                3:'CantorNatom10KTemp300KMultipleRates/Rate12', 
                 4:'test-mpi3rd', #nicocrNatom10KTemp300KMultipleRates/Rate0', 
                }[3]
     sourcePath = os.getcwd() +\
@@ -103,7 +103,7 @@ if __name__ == '__main__':
     def SetVariables():
         Variable = {
                 0:' -var natoms 100000 -var cutoff 3.52 -var ParseData 0 -var ntype 3 -var DumpFile dumpInit.xyz -var WriteData data_init.txt',
-                6:' -var buff 0.0 -var T 300 -var P 0.0 -var gammaxy 1.0 -var gammadot 0.25e-4 -var nthermo 10000 -var ndump 1000 -var ParseData 1 -var DataFile equilibrated.dat -var DumpFile dumpSheared.xyz',
+                6:' -var buff 0.0 -var T 300 -var P 0.0 -var gammaxy 1.0 -var gammadot 0.125e-4 -var nthermo 10000 -var ndump 1000 -var ParseData 1 -var DataFile equilibrated.dat -var DumpFile dumpSheared.xyz',
                 4:' -var T 600.0 -var t_sw 20.0 -var DataFile Equilibrated_600.dat -var nevery 100 -var ParseData 1 -var WriteData swapped_600.dat', 
                 5:' -var buff 0.0 -var nevery 1000 -var ParseData 0 -var natoms 10000 -var ntype 5 -var cutoff 3.54  -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt -var seed0 %s -var seed1 %s -var seed2 %s -var seed3 %s'%tuple(np.random.randint(1001,9999,size=4)), 
                 51:' -var buff 0.0 -var nevery 1000 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt', 
@@ -133,7 +133,7 @@ if __name__ == '__main__':
     EXEC_lmp = ['lmp_mpi','lmp_serial','lmp_intel_cpu_intelmpi'][2]
     durtn = ['95:59:59','01:59:59','167:59:59'][ 2 ]
     mem = '16gb'
-    partition = ['gpu-v100','parallel','cpu2019','single'][2]
+    partition = ['gpu-v100','parallel','cpu2019','single'][1]
     #--
     DeleteExistingFolder = True 
 
