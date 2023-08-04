e7bc7b9adf3ca77b978f91434aa602752b943ab8
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-08-04 18:39:28 +0200

    update

--- tensile/simulations/simulations.py
+++ tensile/simulations/simulations.py
@@ -42,11 +42,11 @@ if __name__ == '__main__':
 
     nruns	 = range(24)
     #
-    nThreads =  8 #32 #16 #4 #8
+    nThreads =  12 #8 #32 #16 #4 #8
     nNode	 = 1
     #
     jobname  = {
-                3:'CantorNatom10KTemp300KMultipleRates/Rate11', 
+                3:'CantorNatom10KTemp300KMultipleRates/Rate13', 
                 4:'test-mpi3rd', #nicocrNatom10KTemp300KMultipleRates/Rate0', 
                }[3]
     sourcePath = os.getcwd() +\
@@ -103,7 +103,7 @@ if __name__ == '__main__':
     def SetVariables():
         Variable = {
                 0:' -var natoms 100000 -var cutoff 3.52 -var ParseData 0 -var ntype 3 -var DumpFile dumpInit.xyz -var WriteData data_init.txt',
-                6:' -var buff 0.0 -var T 300 -var P 0.0 -var gammaxy 1.0 -var gammadot 0.25e-4 -var nthermo 10000 -var ndump 1000 -var ParseData 1 -var DataFile equilibrated.dat -var DumpFile dumpSheared.xyz',
+                6:' -var buff 0.0 -var T 300 -var P 0.0 -var gammaxy 1.0 -var gammadot 0.0625e-4 -var nthermo 10000 -var ndump 1000 -var ParseData 1 -var DataFile equilibrated.dat -var DumpFile dumpSheared.xyz',
                 4:' -var T 600.0 -var t_sw 20.0 -var DataFile Equilibrated_600.dat -var nevery 100 -var ParseData 1 -var WriteData swapped_600.dat', 
                 5:' -var buff 0.0 -var nevery 1000 -var ParseData 0 -var natoms 10000 -var ntype 5 -var cutoff 3.54  -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt -var seed0 %s -var seed1 %s -var seed2 %s -var seed3 %s'%tuple(np.random.randint(1001,9999,size=4)), 
                 51:' -var buff 0.0 -var nevery 1000 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt', 
