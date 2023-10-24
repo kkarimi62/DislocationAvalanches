abc33249db44d2ab11d8de46b5c69141a81b4528
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-10-25 00:58:34 +0200

    update

--- nanoindentation/ebsd/ebsd_matlab_irradiated.m
+++ nanoindentation/ebsd/ebsd_matlab_irradiated.m
@@ -33,6 +33,64 @@ text(big_grains,int2str(big_grains.id),'FontSize', 4);
 saveas(h,'grainsBitmap/grains','png');
 %corresponding grain id
 
+
+% a key the colorizes according to misorientation angle and axis
+ipfKey = axisAngleColorKey(ebsd);
+
+% set the grain mean orientations as reference orinetations
+ipfKey.oriRef = grains(ebsd('indexed').grainId).meanOrientation;
+
+% plot the data
+h = figure;
+plot(ebsd('indexed'),ipfKey.orientation2color(ebsd('indexed').orientations),'micronBar','off','figSize','medium')
+plot(grains.boundary,'linewidth',2)
+saveas(h,'grainsBitmap/misorientationAngle','png');
+
+% denoise orientation data
+F = halfQuadraticFilter;
+
+ebsd = smooth(ebsd('indexed'),F,'fill',grains);
+
+% plot the denoised data
+h = figure;
+ipfKey.oriRef = grains(ebsd('indexed').grainId).meanOrientation;
+plot(ebsd('indexed'),ipfKey.orientation2color(ebsd('indexed').orientations),'micronBar','off','figSize','medium')
+plot(grains.boundary,'linewidth',2)
+saveas(h,'grainsBitmap/misorientationAngleSmooth','png');
+
+
+
+% consider only the Fe(alpha) phase
+ebsd = ebsd('indexed').gridify;
+
+% compute the curvature tensor
+kappa = ebsd.curvature
+
+% one can index the curvature tensors in the same way as the EBSD data.
+% E.g. the curvature in pixel (2,3) is
+kappa(2,3)
+
+
+newMtexFigure('nrows',3,'ncols',3);
+
+% cycle through all components of the tensor
+h = figure;
+for i = 1:3
+  for j = 1:3
+
+    nextAxis(i,j)
+    plot(ebsd,kappa{i,j},'micronBar','off');
+    plot(grains.boundary,'linewidth',2);
+
+  end
+end
+
+% unify the color rage  - you may also use setColoRange equal
+setColorRange([-0.005,0.005])
+drawNow(gcm,'figSize','large')
+saveas(h,'grainsBitmap/gnd','png');
+
+
 %select the corresponding EBSD data
 %fileName = [mpath filesep 'grain_labels_10mN.txt'];
 %indentLabels = dlmread(fileName,'',1,0);
