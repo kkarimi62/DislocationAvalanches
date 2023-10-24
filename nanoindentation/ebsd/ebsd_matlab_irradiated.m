26436528d1258512ca7564b5d7a3792f492c318f
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-10-25 01:04:05 +0200

    update

--- nanoindentation/ebsd/ebsd_matlab_irradiated.m
+++ nanoindentation/ebsd/ebsd_matlab_irradiated.m
@@ -5,33 +5,36 @@ mpath='/home/kamran.karimi1/Project/git/DislocationAvalanches/irradiation/ebsd/i
 fileName = [mpath filesep 'EBSD_304And316L/316L virgin.ang'];
 %fileName = [mpath filesep 'EBSD_304And316L/316L_01 dpa He 60 keV.ang'];
 
+% set up the plotting convention
+plotx2north
 
-% load file
+% import the EBSD data
 ebsd = EBSD.load(fileName,'convertSpatial2EulerReferenceFrame','setting 2')
-ebsd = ebsd('indexed'); %only indexed grain
-[grains,ebsd.grainId] = calcGrains(ebsd);
-keyboard;
 
-%grains = calcGrains(ebsd('indexed'),'theshold',10*degree);
+% define the color key
+ipfKey = ipfHSVKey(ebsd);
+ipfKey.inversePoleFigureDirection = yvector;
 
-% perform grain segmentation (only the very big grains)
-big_grains = grains(grains.grainSize>1);
-
-
-%--- plot grains with labels
+% and plot the orientation data
 h = figure;
-% plot them
-plot(big_grains,big_grains.meanOrientation,'micronbar','off', 'coordinates','on');
-lim = axis;
-% plot on top their ids
-text(big_grains,int2str(big_grains.id),'FontSize', 4);
-% add indenter
-%fileName = [mpath filesep 'input/r_indenters.txt'];
-%r_indenters = dlmread(fileName,'',1,0);
-%plot indenters
-%text(r_indenters(:,1),r_indenters(:,2),'.','Color','red');
+plot(ebsd,ipfKey.orientation2color(ebsd.orientations),'micronBar','off','figSize','medium')
 saveas(h,'grainsBitmap/grains','png');
-%corresponding grain id
+
+% reconstruct grains
+[grains,ebsd.grainId] = calcGrains(ebsd,'angle',5*degree);
+
+% remove small grains
+ebsd(grains(grains.grainSize<=5)) = [];
+
+% redo grain reconstruction
+[grains,ebsd.grainId] = calcGrains(ebsd,'angle',2.5*degree);
+
+% smooth grain boundaries
+grains = smooth(grains,5);
+h = figure;
+plot(grains.boundary,'linewidth',2)
+saveas(h,'grainsBitmap/grainsSmooth','png');
+
 
 
 % a key the colorizes according to misorientation angle and axis
@@ -91,98 +94,5 @@ drawNow(gcm,'figSize','large')
 saveas(h,'grainsBitmap/gnd','png');
 
 
-%select the corresponding EBSD data
-%fileName = [mpath filesep 'grain_labels_10mN.txt'];
-%indentLabels = dlmread(fileName,'',1,0);
-%for i = 1:size(indentLabels,1)
-%	id = indentLabels(i,2);
-% some function
-%	ebsd_maxGrain = ebsd(grains(id));
-	% plot it
-%	h = figure;
-%	plot(ebsd_maxGrain,'faceColor','white','micronbar','off','legend','off');
-	% plot the grain boundary on top
-%	hold on
-%	plot(grains(id).boundary,'linewidth',1)
-%	hold off
-	% center plot
-%	xc=-3.630098e+01;yc=-5.368517e+01;
-%   xlo=lim(1);xhi=lim(2);ylo=lim(3);yhi=lim(4);
-%	axis(lim);
-%	legend('off');
-%	saveas(h,sprintf('grainsBitmap/grain_label%i',id),'png');
-%end
 
-
-%plot indenter tip
-
-% misorientation
-%pairs = grains.neighbors;
-%mori = inv(grains(pairs(:,1)).meanOrientation) .* grains(pairs(:,2)).meanOrientation;
-
-
-% length of the common boundary between grain i & j
-%seg_length=[];
-%for ipair = 1:size(pairs,1)
-%	grain_i = pairs(ipair,1);
-%	grain_j = pairs(ipair,2);
-%	assert(grain_i < grain_j);
-%	filtr = grains(grain_i).boundary.grainId(:,2) == grain_j;
-%	sizee = grains(grain_i).boundary.segLength;
-%	size_filtr = sizee(filtr);
-%	seg_length(ipair) = sum(size_filtr);
-%end
-
-%---- print attributes
-% open your file for writing
-%fid = fopen('output/attributes.txt','wt');
-%fprintf(fid,'#grainID x y area perimeter subBoundaryLength diameter equivalentPerimeter shapeFactor isBoundary hasHole isInclusion numNeighbors phi1 Phi phi2\n');
-%fprintf(fid,'%d %e %e %e %e %e %e %e %e %d %d %d %d %e %e %e\n', transpose([grains.id grains.centroid grains.area grains.perimeter grains.subBoundaryLength grains.diameter grains.equivalentPerimeter grains.shapeFactor grains.isBoundary grains.hasHole grains.isInclusion grains.numNeighbors grains.meanOrientation.phi1./degree grains.meanOrientation.Phi./degree grains.meanOrientation.phi2./degree]));
-%fclose(fid);
-
-% open your file for writing
-%fid = fopen('output/EulerAngles.txt','wt');
-%fprintf(fid,'#phi1 Phi phi2\n');
-%fprintf(fid,'%e %e %e\n', transpose([ebsd.orientations.phi1./degree, ebsd.orientations.Phi./degree, ebsd.orientations.phi2./degree ]));
-%fclose(fid);
-
-% misorientation
-%fid = fopen('output/misOrientationAngle.txt','wt');
-%fprintf(fid,'#grain_i_ID grain_j_ID phi1 Phi phi2 angle\n');
-%fprintf(fid,'%d %d %e %e %e %e\n', transpose([ pairs mori.phi1./degree mori.Phi./degree mori.phi2./degree mori.angle./degree] ));
-%fclose(fid);
-
-
-% edge attributes
-%fid = fopen('output/pairwise_attributes.txt','wt');
-%fprintf(fid,'#grain_i_ID grain_j_ID misOrientationAngle(deg) boundaryLength(micron)\n');
-%fprintf(fid,'%d %d %e %e\n', transpose([ pairs mori.angle./degree transpose(seg_length)] ));
-%fclose(fid);
-
-% indenters and corresponding grains
-%ids=[];lid=[];label=[];
-%for i = 1:size(r_indenters,1)
-%	ids(i) = grains(r_indenters(i,3),r_indenters(i,4)).id;
-%	lid(i) = r_indenters(i,1);
-%	label(i) = r_indenters(i,2)+1;
-%end
-%length(ids)
-%length(r_indenters)
-%ids = grains(r_indenters(:,1),r_indenters(:,2)).id;
-%length(ids)
-
-%fid = fopen('output/indenter_grainID.txt','wt');
-%fprintf(fid,'#loadID label grainID\n');
-%fprintf(fid,'%d %d %d\n', ([ lid; label; ids; ]));
-%fclose(fid);
-
-% pixel-based ebsd id
-%ebsd = ebsd.gridify;
-%A = ebsd.grainId;
-%fid = fopen('output/id_matrix.txt','wt');
-%for ii = 1:size(A,1)
-%    fprintf(fid,'%d\t',A(ii,:));
-%    fprintf(fid,'\n');
-%end
-%fclose(fid)
 
