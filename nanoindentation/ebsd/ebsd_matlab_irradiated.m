5af07619f6af1a47f3f3cb454d989945e001eba8
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-10-25 11:19:38 +0200

    update

--- nanoindentation/ebsd/ebsd_matlab_irradiated.m
+++ nanoindentation/ebsd/ebsd_matlab_irradiated.m
@@ -16,9 +16,9 @@ ipfKey = ipfHSVKey(ebsd);
 ipfKey.inversePoleFigureDirection = yvector;
 
 % and plot the orientation data
-figure(1);
-plot(ebsd,ipfKey.orientation2color(ebsd.orientations),'micronBar','off','figSize','medium')
-saveas(gca,'grainsBitmap/grains','png');
+%h = figure(1);
+%plot(ebsd,ipfKey.orientation2color(ebsd.orientations),'micronBar','off','figSize','medium')
+%saveas(h,'grainsBitmap/grains','png');
 
 % reconstruct grains
 [grains,ebsd.grainId] = calcGrains(ebsd,'angle',5*degree);
@@ -31,67 +31,12 @@ ebsd(grains(grains.grainSize<=5)) = [];
 
 % smooth grain boundaries
 grains = smooth(grains,5);
-figure(2);
+h=figure;
 plot(grains.boundary,'linewidth',2)
-saveas(gca,'grainsBitmap/grainsSmooth','png');
+saveas(h,'grainsBitmap/grainsSmooth','png');
 
 
 
-% a key the colorizes according to misorientation angle and axis
-ipfKey = axisAngleColorKey(ebsd);
-
-% set the grain mean orientations as reference orinetations
-ipfKey.oriRef = grains(ebsd('indexed').grainId).meanOrientation;
-
-% plot the data
-h3 = figure;
-plot(ebsd('indexed'),ipfKey.orientation2color(ebsd('indexed').orientations),'micronBar','off','figSize','medium')
-plot(grains.boundary,'linewidth',2)
-saveas(h3,'grainsBitmap/misorientationAngle','png');
-
-% denoise orientation data
-F = halfQuadraticFilter;
-
-ebsd = smooth(ebsd('indexed'),F,'fill',grains);
-
-% plot the denoised data
-h4 = figure;
-ipfKey.oriRef = grains(ebsd('indexed').grainId).meanOrientation;
-plot(ebsd('indexed'),ipfKey.orientation2color(ebsd('indexed').orientations),'micronBar','off','figSize','medium')
-plot(grains.boundary,'linewidth',2)
-saveas(h4,'grainsBitmap/misorientationAngleSmooth','png');
-
-
-
-% consider only the Fe(alpha) phase
-ebsd = ebsd('indexed').gridify;
-
-% compute the curvature tensor
-kappa = ebsd.curvature
-
-% one can index the curvature tensors in the same way as the EBSD data.
-% E.g. the curvature in pixel (2,3) is
-kappa(2,3)
-
-
-newMtexFigure('nrows',3,'ncols',3);
-
-% cycle through all components of the tensor
-h5 = figure;
-for i = 1:3
-  for j = 1:3
-
-    nextAxis(i,j)
-    plot(ebsd,kappa{i,j},'micronBar','off');
-    plot(grains.boundary,'linewidth',2);
-
-  end
-end
-
-% unify the color rage  - you may also use setColoRange equal
-setColorRange([-0.005,0.005])
-drawNow(gcm,'figSize','large')
-saveas(h5,'grainsBitmap/gnd','png');
 
 
 
