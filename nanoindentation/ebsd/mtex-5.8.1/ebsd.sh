c69674f97b95d55915584e138dd93bcb39dc5287
kamran.karimi1 <kkarimi62>
2023-10-26 08:50:12 -0600

    updates

--- nanoindentation/ebsd/mtex-5.8.1/ebsd.sh
+++ nanoindentation/ebsd/mtex-5.8.1/ebsd.sh
@@ -2,8 +2,10 @@
 exec=/home/kamran.karimi1/Project/git/DislocationAvalanches/nanoindentation/ebsd
 module load matlab/r2017b
 
-python ${exec}/indent_xy.py
-#-nojvm -nosplash -nodesktop -r
+#python ${exec}/indent_xy.py
 matlab -nodisplay -r "try, run('${exec}/ebsd_matlab_irradiated'), catch e, disp(getReport(e)), exit(1), end, exit(0);"
+echo "matlab exit code: $?"
+
+matlab -nodisplay -r "try, run('${exec}/fprintGrainAttributes'), catch e, disp(getReport(e)), exit(1), end, exit(0);"
 echo "matlab exit code: $?"
 
