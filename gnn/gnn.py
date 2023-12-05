451737054c32e7ec44d12474992abb49d3ca636a
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-12-05 17:00:24 +0100

    update

--- gnn/gnn.py
+++ gnn/gnn.py
@@ -1,89 +1,95 @@
 from backports import configparser
 
 def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL,seed):
-	#--- parse conf. file
-	confParser = configparser.ConfigParser()
-	confParser.read('config.ini')
-	#--- set parameters
-	confParser.set('Parameters','num_processing_steps_tr','3')
-	confParser.set('Parameters','num_training_iterations','100000')
-	confParser.set('Parameters','learning_rate','1.0e-03')
-	confParser.set('Parameters','attributes','x    y    area    perimeter    subBoundaryLength  diameter    equivalentPerimeter    shapeFactor    isBoundary  hasHole    isInclusion    numNeighbors phi1 Phi phi2') 
-	confParser.set('Parameters','train_size_learning','1.0')
-	confParser.set('Parameters','stopping_criterion','1')
-	confParser.set('Parameters','n_cross_val','4')
-	confParser.set('Parameters','seed','%s'%seed)
-	confParser.set('Parameters','split_type','0')
-	#
-	confParser.set('flags','train_test','False')
-	confParser.set('flags','learning_curve','False')
-	confParser.set('flags','validation_curve','True')
-	confParser.set('flags','remote_machine','True')
-	#
-	confParser.set('gnn library path','gnnLibDir',os.getcwd()+'/./hs_implementation')
-	#
-	confParser.set('python libarary path','pyLibDir',os.getcwd()+'/../../HeaDef/postprocess')
-	#
-	confParser.set('test data files','ebsd_path',os.getcwd()+'/../nanoindentation/ebsd/output')
-	confParser.set('test data files','test_data_file_path',os.getcwd()+'/../nanoindentation/ebsd/output/attributes.txt')
-	confParser.set('test data files','test_data_file_path2nd',os.getcwd()+'/../nanoindentation/ebsd/output/pairwise_attributes.txt')
-	confParser.set('test data files','load_depth_path',os.getcwd()+'/../nanoindentation/loadCurves')
-	#--- write
-	confParser.write(open('config.ini','w'))	
-	#--- set environment variables
-	someFile = open( 'oarScript.sh', 'w' )
-	print('#!/bin/bash\n',file=someFile)
-	print('EXEC_DIR=%s\nmodule load python/anaconda3-2018.12\nsource /global/software/anaconda/anaconda3-2018.12/etc/profile.d/conda.sh\nconda activate gnnEnv2nd '%( EXEC_DIR ),file=someFile)
+    #--- parse conf. file
+    confParser = configparser.ConfigParser()
+    confParser.read('config_irradiated.ini')
+    #--- set parameters
+#     confParser.set('Parameters','num_processing_steps_tr','3')
+#     confParser.set('Parameters','num_training_iterations','100000')
+#     confParser.set('Parameters','learning_rate','1.0e-03')
+#     confParser.set('Parameters','attributes','x    y    area    perimeter    subBoundaryLength  diameter    equivalentPerimeter    shapeFactor    isBoundary  hasHole    isInclusion    numNeighbors phi1 Phi phi2') 
+#     confParser.set('Parameters','train_size_learning','1.0')
+#     confParser.set('Parameters','stopping_criterion','1')
+#     confParser.set('Parameters','n_cross_val','4')
+#     confParser.set('Parameters','seed','%s'%seed)
+#     confParser.set('Parameters','split_type','0')
+#     #
+#     confParser.set('flags','train_test','False')
+#     confParser.set('flags','learning_curve','False')
+#     confParser.set('flags','validation_curve','True')
+#     confParser.set('flags','remote_machine','True')
+    #
+    confParser.set('gnn library path','gnnLibDir',os.getcwd()+'/./hs_implementation')
+    #
+    confParser.set('python libarary path','pyLibDir',os.getcwd()+'/../../HeaDef/postprocess')
+    #
+    confParser.set('test data files','ebsd_path',os.getcwd()+'/../nanoindentation/ebsd/output/before_irradiation')
+    confParser.set('test data files','test_data_file_path',os.getcwd()+'/../nanoindentation/ebsd/output/before_irradiation/attributes.txt')
+    confParser.set('test data files', 'test_data_file_path2nd', os.getcwd()+'/../nanoindentation/ebsd/output/before_irradiation/pairwise_attributes.txt')
+    #
+    confParser.set('Dislocation Density','alpha','alpha11')
+    confParser.set('Dislocation Density','qlo','0.0')
+    confParser.set('Dislocation Density','qhi','1.0')
+    
+    #--- write
+    confParser.write(open('config_irradiated.ini','w'))
+    #--- set environment variables
+    someFile = open( 'oarScript.sh', 'w' )
+    print('#!/bin/bash\n',file=someFile)
+    print('EXEC_DIR=%s\nmodule load python/anaconda3-2018.12\nsource /global/software/anaconda/anaconda3-2018.12/etc/profile.d/conda.sh\nconda activate gnnEnv2nd '%( EXEC_DIR ),file=someFile)
 
-	if convert_to_py:
-		print('ipython3 py_script.py\n',file=someFile)
-	else:	 
-		print('jupyter nbconvert --execute $EXEC_DIR/%s --to html --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html'%(PYFIL), file=someFile)
+    if convert_to_py:
+        print('ipython3 py_script.py\n',file=someFile)
+    else:	 
+        print('jupyter nbconvert --execute $EXEC_DIR/%s --to html --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html'%(PYFIL), file=someFile)
 #	print('ipython %s'%(PYFIL), file=someFile)
-	someFile.close()										  
+    someFile.close()										  
 #
 if __name__ == '__main__':
-	import os
-	import numpy as np
-	##
-	nruns	 = range(1)
-	jobname  = {
-					1:'learning_rate',
-					2:'model_validation', #'learning_curve',
-				}[ 2 ]
-	DeleteExistingFolder = True
-	EXEC_DIR = '.'     #--- path for executable file
-	durtn = '23:59:59'
-	mem = '10gb'
-	partition = ['cpu2019','bigmem','parallel','single'][1]
-	PYFILdic = { 
-		0:'gnnPolyCryst.ipynb',
-		}
-	keyno = 0
-	convert_to_py = True
+    import os
+    import numpy as np
+    ##
+    nruns	 = range(1)
+    jobname  = {
+                    1:'learning_rate',
+                    2:'model_validation', #'learning_curve',
+                    3:'gndIradiation/before', #'learning_curve',
+                }[ 3 ]
+    DeleteExistingFolder = True
+    EXEC_DIR = '.'     #--- path for executable file
+    durtn = '23:59:59'
+    mem = '10gb'
+    partition = ['cpu2019','bigmem','parallel','single'][1]
+    PYFILdic = { 
+        0:'gnnPolyCryst.ipynb',
+        1:'gnnPolyCryst_irradiated.ipynb',
+        }
+    keyno = 1
+    convert_to_py = True
 #---
 #---
-	PYFIL = PYFILdic[ keyno ] 
-	#--- update argV
-	if convert_to_py:
-		os.system('jupyter nbconvert --to script %s --output py_script\n'%PYFIL)
-		PYFIL = 'py_script.py'
-	#---
-	if DeleteExistingFolder:
-		os.system( 'rm -rf %s' % jobname ) # --- rm existing
-	# --- loop for submitting multiple jobs
-	counter = init = 0
-	for counter in nruns:
-		init = counter
-		print(' i = %s' % counter)
-		writPath = os.getcwd() + '/%s/Run%s' % ( jobname, counter ) # --- curr. dir
-		os.system( 'mkdir -p %s' % ( writPath ) ) # --- create folder
-		seed = 128 #np.random.randint(100,1000)
-		makeOAR( writPath, 1, 1, durtn, PYFIL,seed) # --- make oar script
-		os.system( 'chmod +x oarScript.sh; cp oarScript.sh config.ini %s; cp %s/%s %s' % ( writPath, EXEC_DIR, PYFIL, writPath ) ) # --- create folder & mv oar scrip & cp executable
-		os.system( 'sbatch --partition=%s --mem=%s --time=%s --job-name %s.%s --output %s.%s.out --error %s.%s.err \
-						    --chdir %s -c %s -n %s %s/oarScript.sh'\
-						   % ( partition, mem, durtn, jobname, counter, jobname, counter, jobname, counter \
-						       , writPath, 1, 1, writPath ) ) # --- runs oarScript.sh!
-											 
+    PYFIL = PYFILdic[ keyno ] 
+    #--- update argV
+    if convert_to_py:
+        os.system('jupyter nbconvert --to script %s --output py_script\n'%PYFIL)
+        PYFIL = 'py_script.py'
+    #---
+    if DeleteExistingFolder:
+        os.system( 'rm -rf %s' % jobname ) # --- rm existing
+    # --- loop for submitting multiple jobs
+    counter = init = 0
+    for counter in nruns:
+        init = counter
+        print(' i = %s' % counter)
+        writPath = os.getcwd() + '/%s/Run%s' % ( jobname, counter ) # --- curr. dir
+        os.system( 'mkdir -p %s' % ( writPath ) ) # --- create folder
+        seed = 128 #np.random.randint(100,1000)
+        makeOAR( writPath, 1, 1, durtn, PYFIL,seed) # --- make oar script
+        os.system( 'chmod +x oarScript.sh; cp oarScript.sh config*.ini %s; cp %s/%s %s' % ( writPath, EXEC_DIR, PYFIL, writPath ) ) # --- create folder & mv oar scrip & cp executable
+        os.system( 'sbatch --partition=%s --mem=%s --time=%s --job-name %s.%s --output %s.%s.out --error %s.%s.err \
+                            --chdir %s -c %s -n %s %s/oarScript.sh'\
+                           % ( partition, mem, durtn, jobname, counter, jobname, counter, jobname, counter \
+                               , writPath, 1, 1, writPath ) ) # --- runs oarScript.sh!
+
 
--- gnn/submitPY.py
+++ gnn/submitPY.py
@@ -1,29 +1,51 @@
 if __name__ == '__main__':
-	import sys
-	import os
-	import numpy as np
-	from itertools import combinations
-	#---
-	lnums = [ 52, 12 ]
-	string=open('gnn.py').readlines() #--- python script
-	#---
-	train_size_learning=np.arange(0.1,1.1,0.1)
+    import sys
+    import os
+    import numpy as np
+    from itertools import combinations
+    #---
+    lnums = [ 57, 27, 28, 29, 31, 32, 33 ]
+    string=open('gnn.py').readlines() #--- python script
+    #---
+    Alpha = dict(zip(range(100),'alpha11 alpha12 alpha13 alpha21 alpha22 alpha23 alpha31 alpha32 alpha33'.split()))
+    Qlo   = dict(zip(range(100),[0,25,50,75]))
+    Qhi   = dict(zip(range(100),[25,50,75,100]))
+    irradiate = dict(zip(range(100),['before','after']))
+    #---
+    count = 0
+    for key_alpha in Alpha:
+        alpha = Alpha[key_alpha]
+        for key_q in Qlo:
+            qlo = Qlo[ key_q ]
+            qhi = Qhi[ key_q ]
+            for key_i in irradiate:
+                str_irradiate = irradiate[ key_i ]
+            #---	
+                inums = lnums[ 0 ] - 1
+                string[ inums ] = "\t3:\'gnd/%s/q%s/alpha%s\',\n" % (str_irradiate,key_q,key_alpha) #--- change job name
+                #---	densities
+                inums = lnums[ 1 ] - 1
+                path  = os.getcwd()+'/../nanoindentation/ebsd/output/%s_irradiation'%str_irradiate
+                string[ inums ] = "\tconfParser.set(\'test data files\',\'ebsd_path\',\'%s\')\n"%(path)
+    confParser.set('','',)
+                #---
+                inums = lnums[ 2 ] - 1
+                string[ inums ] = "\tconfParser.set(\'test data files\',\'test_data_file_path\',\'%s/attributes.txt\')\n"%(path)
+                #---
+                inums = lnums[ 3 ] - 1
+                string[ inums ] = "\tconfParser.set(\'test data files\',\'test_data_file_path2nd\',\'%s/pairwise_attributes.txt\')\n"%(path)
+                #---
+                inums = lnums[ 4 ] - 1
+                string[ inums ] = "\tconfParser.set(\'Dislocation Density\',\'alpha\',\'%s\')\n"%(alpha)
+                #---
+                inums = lnums[ 5 ] - 1
+                string[ inums ] = "\tconfParser.set(\'Dislocation Density\',\'qlo\',\'%s\')\n"%(qlo)
+                #---
+                inums = lnums[ 6 ] - 1
+                string[ inums ] = "\tconfParser.set(\'Dislocation Density\',\'qhi\',\'%s\')\n"%(qhi)
 
-	PHI=dict(zip(range(len(train_size_learning)),train_size_learning))
-	nphi = len(PHI)
-	#---
-	count = 0
-	for key in PHI:
-		val = PHI[key]
-		#---	
-		inums = lnums[ 0 ] - 1
-		string[ inums ] = "\t2:\'model_validation%s\',\n" % (key) #--- change job name
-		#---	densities
-		inums = lnums[ 1 ] - 1
-		string[ inums ] = "\tconfParser.set(\'Parameters\',\'train_size_learning\',\'%s\')\n"%(val)
 
-
-		sfile=open('junk%s.py'%count,'w');sfile.writelines(string);sfile.close()
-		os.system( 'python3 junk%s.py'%count )
-		os.system( 'rm junk%s.py'%count )
-		count += 1
+                sfile=open('junk%s.py'%count,'w');sfile.writelines(string);sfile.close()
+                os.system( 'python3 junk%s.py'%count )
+                os.system( 'rm junk%s.py'%count )
+                count += 1
